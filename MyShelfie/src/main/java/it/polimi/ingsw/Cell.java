8e9c7824e5da8c3e60ae4b1de3da94f59ef577fe
Angelo <angelodr3010@gmail.com>
2023-03-31 16:24:16 +0200

    Enumerazione Tiles e Classe Cell con i metodi isEmpty() e isValid()

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/Cell.java
@@ -0,0 +1,39 @@
+package it.polimi.ingsw;
+/**
+ * Class: Cell
+ * @author Angelo Di Rosa
+ * This class represents the little square of the board where you put the object tile.
+ *
+ */
+public class Cell {
+
+    private Tiles tile;
+
+    public Cell (Tiles t){
+        this.tile=t;
+    }
+    /**
+     * Method: isValid()
+     * @author Angelo Di Rosa
+     * isValid() is used to check if the single cell of the board can be used for the game or not.
+     * This because the board available for the game is not a full 9x9 matrix,
+     * so with this method we check if we can put a tile on the cell.*/
+    public boolean isValid(){
+        if (tile == Tiles.NOTVALID){
+            return false;
+        }
+        return true;
+    }
+    /**
+     * Method: isEmpty()
+     * @author Angelo Di Rosa
+     * isEmpty checks if the cell is occupied by a tile or not*/
+    public boolean isEmpty(){
+        for (Tiles t : Tiles.values()){
+            if (tile == t){
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ MyShelfie/src/main/java/it/polimi/ingsw/Tiles.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw;
+/**
+ * Enumeration : Tiles
+ * @author Angelo Di Rosa
+ * This enumeration is the abstraction of the different types of Object tiles from the game. The Object tiles are differentiated by colors.*/
+
+public enum Tiles {
+        BLUE,
+        GREEN,
+        PURPLE,
+        YELLOW,
+        WHITE,
+        LIGHTBLUE,
+        NOTVALID
+}
+
