9970496f6cde771721859b389b6e7c3e2141259f
mohammad <10328457@mail.polimi.it>
2023-04-22 13:14:00 +0200

    Add comments
    Also fix tests

--- MyShelfie/src/main/java/it/polimi/ingsw/model/PersonalGoal.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/model/PersonalGoal.java
@@ -62,18 +62,39 @@ public class PersonalGoal implements Goal, Serializable {
                 new HashMap<>(Map.of(
                         Pair.of(5, 0), Tiles.PURPLE, Pair.of(5, 2), Tiles.BLUE,   Pair.of(4, 4), Tiles.GREEN,
                         Pair.of(3, 3), Tiles.WHITE,  Pair.of(2, 1), Tiles.YELLOW, Pair.of(0, 2), Tiles.LIGHTBLUE)),
-                // TODO: Fare le altre come la prima
-                new HashMap<>(Map.of(Pair.of(0, 0), Tiles.PURPLE, Pair.of(1, 0), Tiles.PURPLE)),
-                new HashMap<>(Map.of(Pair.of(0, 0), Tiles.PURPLE, Pair.of(1, 0), Tiles.PURPLE)),
-                new HashMap<>(Map.of(Pair.of(0, 0), Tiles.PURPLE, Pair.of(1, 0), Tiles.PURPLE)),
-                new HashMap<>(Map.of(Pair.of(0, 0), Tiles.PURPLE, Pair.of(1, 0), Tiles.PURPLE)),
-                new HashMap<>(Map.of(Pair.of(0, 0), Tiles.PURPLE, Pair.of(1, 0), Tiles.PURPLE)),
-                new HashMap<>(Map.of(Pair.of(0, 0), Tiles.PURPLE, Pair.of(1, 0), Tiles.PURPLE)),
-                new HashMap<>(Map.of(Pair.of(0, 0), Tiles.PURPLE, Pair.of(1, 0), Tiles.PURPLE)),
-                new HashMap<>(Map.of(Pair.of(0, 0), Tiles.PURPLE, Pair.of(1, 0), Tiles.PURPLE)),
-                new HashMap<>(Map.of(Pair.of(0, 0), Tiles.PURPLE, Pair.of(1, 0), Tiles.PURPLE)),
-                new HashMap<>(Map.of(Pair.of(0, 0), Tiles.PURPLE, Pair.of(1, 0), Tiles.PURPLE)),
-                new HashMap<>(Map.of(Pair.of(0, 0), Tiles.PURPLE, Pair.of(1, 0), Tiles.PURPLE))
+                new HashMap<>(Map.of(
+                        Pair.of(3, 0), Tiles.GREEN, Pair.of(4, 1), Tiles.PURPLE,   Pair.of(3, 2), Tiles.YELLOW,
+                        Pair.of(2, 4), Tiles.WHITE,  Pair.of(1, 3), Tiles.LIGHTBLUE, Pair.of(0, 4), Tiles.BLUE)),
+                new HashMap<>(Map.of(
+                        Pair.of(4, 0), Tiles.BLUE, Pair.of(4, 3), Tiles.YELLOW,   Pair.of(3, 2), Tiles.PURPLE,
+                        Pair.of(2, 1), Tiles.GREEN,  Pair.of(2, 4), Tiles.LIGHTBLUE, Pair.of(0, 0), Tiles.WHITE)),
+                new HashMap<>(Map.of(
+                        Pair.of(5, 4), Tiles.YELLOW, Pair.of(3, 0), Tiles.LIGHTBLUE,   Pair.of(3, 2), Tiles.BLUE,
+                        Pair.of(2, 3), Tiles.PURPLE,  Pair.of(1, 1), Tiles.WHITE, Pair.of(1, 2), Tiles.GREEN)),
+                new HashMap<>(Map.of(
+                        Pair.of(4, 1), Tiles.LIGHTBLUE, Pair.of(2, 1), Tiles.BLUE,   Pair.of(2, 2), Tiles.WHITE,
+                        Pair.of(1, 4), Tiles.PURPLE,  Pair.of(0, 0), Tiles.YELLOW, Pair.of(0, 3), Tiles.GREEN)),
+                new HashMap<>(Map.of(
+                        Pair.of(5, 2), Tiles.LIGHTBLUE, Pair.of(5, 4), Tiles.GREEN,   Pair.of(3, 3), Tiles.WHITE,
+                        Pair.of(1, 1), Tiles.YELLOW,  Pair.of(1, 3), Tiles.BLUE, Pair.of(0, 0), Tiles.PURPLE)),
+                new HashMap<>(Map.of(
+                        Pair.of(5, 0), Tiles.GREEN, Pair.of(4, 3), Tiles.BLUE,   Pair.of(3, 1), Tiles.PURPLE,
+                        Pair.of(2, 0), Tiles.LIGHTBLUE,  Pair.of(1, 4), Tiles.YELLOW, Pair.of(0, 2), Tiles.WHITE)),
+                new HashMap<>(Map.of(
+                        Pair.of(5, 4), Tiles.BLUE, Pair.of(4, 1), Tiles.GREEN,   Pair.of(3, 2), Tiles.LIGHTBLUE,
+                        Pair.of(2, 0), Tiles.PURPLE,  Pair.of(1, 3), Tiles.WHITE, Pair.of(0, 3), Tiles.YELLOW)),
+                new HashMap<>(Map.of(
+                        Pair.of(5, 2), Tiles.YELLOW, Pair.of(3, 2), Tiles.GREEN,   Pair.of(2, 4), Tiles.WHITE,
+                        Pair.of(1, 1), Tiles.LIGHTBLUE,  Pair.of(1, 4), Tiles.PURPLE, Pair.of(0, 0), Tiles.BLUE)),
+                new HashMap<>(Map.of(
+                        Pair.of(5, 4), Tiles.LIGHTBLUE, Pair.of(4, 1), Tiles.YELLOW,   Pair.of(3, 0), Tiles.WHITE,
+                        Pair.of(2, 3), Tiles.GREEN,  Pair.of(1, 1), Tiles.BLUE, Pair.of(0, 3), Tiles.PURPLE)),
+                new HashMap<>(Map.of(
+                        Pair.of(5, 2), Tiles.PURPLE, Pair.of(4, 1), Tiles.WHITE,   Pair.of(3, 0), Tiles.YELLOW,
+                        Pair.of(2, 2), Tiles.BLUE,  Pair.of(1, 4), Tiles.GREEN, Pair.of(0, 3), Tiles.LIGHTBLUE)),
+                new HashMap<>(Map.of(
+                        Pair.of(5, 2), Tiles.WHITE, Pair.of(4, 1), Tiles.PURPLE,   Pair.of(3, 2), Tiles.BLUE,
+                        Pair.of(0, 3), Tiles.LIGHTBLUE,  Pair.of(1, 4), Tiles.YELLOW, Pair.of(0, 0), Tiles.GREEN))
         );
 
         var ret = new ArrayList<PersonalGoal>();
