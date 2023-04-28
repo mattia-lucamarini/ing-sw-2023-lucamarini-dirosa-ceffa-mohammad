3be357f6d449d5e88c7353267bd227999074de24
Mattia Lucamarini <mattia.lucamarini@mail.polimi.it>
2023-04-28 13:25:00 +0200

    Added getter and constructor to PersonalGoalCard

--- MyShelfie/src/main/java/it/polimi/ingsw/model/PersonalGoalCard.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/model/PersonalGoalCard.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.model;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Random;
 
@@ -8,23 +9,35 @@ import java.util.Random;
  * This class represents the physical common goal card used in the game.
  * It is like a physical blank card in which we set its value (the personal goal) */
 
-public class PersonalGoalCard extends Card {
+public class PersonalGoalCard extends Card implements Serializable {
     private PersonalGoal goal;
-    static List<PersonalGoal> personalpointer;
+    static List<PersonalGoal> personalpointer = PersonalGoal.all();
 
     /** Method: getGoal()
      * @author Angelo Di Rosa
      * This method overrides the superclass method.
      * It is used to choose a random Personal Goal for the game from a personal goal list.*/
-    @Override
-    public PersonalGoal getGoal(){
-        goal = new PersonalGoal(null, null);
+    public void generateGoal(){
         Random rand = new Random();
-        personalpointer = goal.all();
         int t = rand.nextInt(personalpointer.size());
         goal = personalpointer.get(t);
         personalpointer.remove(t);
-
-        return goal;
+    }
+    public void generateGoal(int index){
+        goal = personalpointer.get(index);
+        personalpointer.remove(index);
+    }
+    public PersonalGoalCard(PersonalGoal goal){
+        this.goal = goal;
+    }
+    public PersonalGoalCard(){
+        generateGoal();
+    }
+    public PersonalGoalCard(int index){
+        generateGoal(index);
+    }
+    @Override
+    public PersonalGoal getGoal(){
+        return this.goal;
     }
 }
