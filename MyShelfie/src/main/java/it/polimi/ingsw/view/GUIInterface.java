093d78e90c55452946760ae11d97b1682670579b
Angelo <angelodr3010@gmail.com>
2023-06-15 22:43:53 +0200

    GUIClient modified, GUIInterface now working properly.

--- MyShelfie/src/main/java/it/polimi/ingsw/view/GUIInterface.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/view/GUIInterface.java
@@ -4,30 +4,88 @@ import it.polimi.ingsw.model.Pair;
 import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.Tiles;
 import it.polimi.ingsw.network.ClientHandler.ClientHandler;
+import it.polimi.ingsw.network.message.Message;
+import it.polimi.ingsw.network.message.NumPlayersMessage;
+import it.polimi.ingsw.utils.ClientDisconnectedException;
+import it.polimi.ingsw.view.MessageView.*;
+import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class GUIInterface implements UserInterface{
     private Stage stage;
     private GUIClient client;
-    private ViewHandler viewhandler;
-    private boolean answertoClient;
-    private ArrayList answers;
-    public GUIInterface(Stage stage, ViewHandler viewhandler) {
+    //private ViewHandler viewhandler;
+    private ConcurrentLinkedQueue<MessageView> sended;
+    private ConcurrentLinkedQueue<MessageView> received;
+    @FXML
+    TextField text;
+
+    /*public GUIInterface(Stage stage, ViewHandler viewhandler) {
         this.stage = stage;
         this.viewhandler = viewhandler;
+    }*/
+
+    public GUIInterface(){
+        sended = new ConcurrentLinkedQueue<>();
+        received = new ConcurrentLinkedQueue<>();
+    }
+
+    public synchronized Player askForUsername() {
+        MessageView message;
+        do{
+             message = received.poll();
+
+        }while(message==null);
+        String username = ((PayloadUsername) message).getUsername();
+        return new Player(username);
+        //mette il messaggio nella struttura d'uscita e aspetta di riceverlo in entrata.
+        //trovato il messaggio e ritorna il valore alla graphic logic.
+
+        /*Platform.runLater(() -> {
+            System.out.println("1");
+            stage.setTitle("MyShelfie!");
+            FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(getClass().getResource("/login.fxml")));
+            loader.setController(this);
+            //viewhandler = loader.getController();
+            Parent login = null;
+            try {
+                login = loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Scene scene1 = new Scene(login, 480, 340);
+            scene1.getStylesheets().add(getClass().getResource("/test_styles.css").toExternalForm());
+            stage.setScene(scene1);
+            stage.show();
+        });*/
+       /* System.out.println("6");
+        Thread t = new Thread(
+                ()-> {username = viewhandler.getUsername();
+                    while(username==null){
+                      viewhandler.getUsername();
+                    System.out.println(viewhandler.getUsername());
+                }
+                    String username = viewhandler.getUsername();});
+        t.start();*/
+
     }
 
     @Override
     public void printErrorMessage(String error) {
-
+        MessageView message = new ErrorMessage(error);
+        sended.add(message);
     }
 
     @Override
@@ -35,18 +93,30 @@ public class GUIInterface implements UserInterface{
 
     }
 
-    public Player askForUsername(){
-        System.out.println("6");
-        viewhandler.getUsername();
-        String username=null;
-        username = viewhandler.getUsername();
-        System.out.println(username);
-        return new Player(username);
-    }
-
     @Override
-    public int askForNumOfPlayers(ClientHandler cl) throws IOException {
-        return viewhandler.getNump();
+    public int askForNumOfPlayers(ClientHandler cl) {
+        MessageView messageSended = new MessageView(MessageCodeView.NEXT_SCENE);
+        System.out.println("ho inviato il messaggio. Adesso aspetto risposta");
+        sended.add(messageSended);
+        MessageView message;
+        do{
+            message = received.poll();
+
+        }while(message==null);
+        int num = ((NumPlayers) message).getNumplayers();
+        System.out.println(((NumPlayers) message).getNumplayers());
+        boolean flag;
+        try {
+            flag = cl.sendingWithRetry(new NumPlayersMessage(num), 2, 1);
+        } catch (ClientDisconnectedException e) { //here if I took much time to send the request
+            System.out.println("Disconnected from the server before sending NumPlayer Message.");
+            return 0;
+        }
+        if (!flag) {
+            System.out.println("Can't send the NumPlayer Message.");
+            return 0;
+        }
+        return num;
     }
 
     @Override
@@ -164,8 +234,10 @@ public class GUIInterface implements UserInterface{
 
     }
 
-    public boolean getAnswer(){
-        return answertoClient;
+    public void addMessage(MessageView message){
+        received.add(message);
     }
-
+    public ConcurrentLinkedQueue<MessageView> getSendedQueue(){
+        return sended;
+    }
 }
--- MyShelfie/src/main/java/it/polimi/ingsw/view/View.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/view/View.java
@@ -25,24 +25,19 @@ public class View extends Application{
 
     public void start(Stage stage) throws IOException, InterruptedException {
         System.out.println("1");
-        stage1=stage;
         stage.setTitle("MyShelfie!");
         FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(getClass().getResource("/login.fxml")));
-        GUIClient client = new GUIClient(this);
-        loader.setController(new ViewHandler(client));
+        //GUIClient client = new GUIClient(this);
+        loader.setController(new ViewHandler(stage,this));
         viewhandler = loader.getController();
         Parent login = loader.load();
         Scene scene1 = new Scene(login, 480, 340);
         scene1.getStylesheets().add(getClass().getResource("/test_styles.css").toExternalForm());
         stage.setScene(scene1);
         stage.show();
-
     }
     public ViewHandler getViewhandler(){
         return viewhandler;
-    }
-    public Stage getStage(){
-        return stage1;
     }
 }
 
